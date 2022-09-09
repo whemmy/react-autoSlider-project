6abcec771c3fa0b9c43492c5ae09a87da9ff29a1
whemmy <67226591+whemmy@users.noreply.github.com>
2022-09-09 06:46:30 +0100

    Final

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React, { useState, useEffect } from 'react'
-import { FiChevronRight, FiChevronLeft } from 'react-icons/fi'
-import { FaQuoteRight } from 'react-icons/fa'
+import Slider from './Slider'
 import data from './data'
 function App() {
   const [people, setPeople] = useState(data)
   const [index, setIndex] = useState(0)
+
+  useEffect(() => {
+    const lastIndex = people.length - 1
+    if (index < 0) {
+      setIndex(lastIndex)
+    }
+    if (index > people.length - 1) {
+      setIndex(0)
+    }
+  }, [index, people])
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1)
+    }, 5000)
+    return () => clearInterval(slider)
+  }, [index])
+
   return (
     <section className="section">
       <div className="title">
@@ -12,26 +29,7 @@ function App() {
           <span></span>reviews
         </h2>
       </div>
-      <div className="section-center">
-        {people.map((person, personIndex) => {
-          const { id, image, name, title, quote } = person
-          return (
-            <article key={id}>
-              <img src={image} alt={name} className="person-img" />
-              <h4>{name}</h4>
-              <p className="title">{title}</p>
-              <p className="text">{quote}</p>
-              <FaQuoteRight className="icon" />
-            </article>
-          )
-        })}
-        <button className="prev">
-          <FiChevronLeft />
-        </button>
-        <button className="next">
-          <FiChevronRight />
-        </button>
-      </div>
+      <Slider />
     </section>
   )
 }
